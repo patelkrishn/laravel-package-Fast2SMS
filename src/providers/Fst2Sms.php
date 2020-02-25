e77b62814e8ecff8de76ad80f0d1082f6606993b
lninfosystem <48345319+lninfosystem@users.noreply.github.com>
2020-02-25 11:44:53 +0530

    some function added

--- src/providers/Fst2Sms.php
+++ src/providers/Fst2Sms.php
@@ -42,8 +42,38 @@ class Fast2Sms extends Fst2SmsProvider
         return $response;
         }
     }
+    public function getTempletId()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://www.fast2sms.com/dev/quick-templates?authorization=".$this->authorization,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "cache-control: no-cache"
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        return "cURL Error #:" . $err;
+        } else {
+        return $response;
+        }
+    }
     public function prepare($params = array()){
-        $field = array(
+        $defaults = array(
             "sender_id" => NULL,
             "language" => NULL,
             "route" => NULL,
@@ -67,6 +97,8 @@ class Fast2Sms extends Fst2SmsProvider
     }
     
     public function send(){
+        
+        // dd($this->parameters);
         if ($this->parameters == null) {
             throw new \Exception("prepare() method not called");
         }
@@ -86,9 +118,9 @@ class Fast2Sms extends Fst2SmsProvider
           CURLOPT_SSL_VERIFYPEER => 0,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => json_encode($field),
+          CURLOPT_POSTFIELDS => json_encode($this->parameters),
           CURLOPT_HTTPHEADER => array(
-            "authorization: YOUR_API_KEY",
+            "authorization: ".$this->authorization,
             "cache-control: no-cache",
             "accept: */*",
             "content-type: application/json"
@@ -101,9 +133,9 @@ class Fast2Sms extends Fst2SmsProvider
         curl_close($curl);
 
         if ($err) {
-          echo "cURL Error #:" . $err;
+          return "cURL Error #:" . $err;
         } else {
-          echo $response;
+          return $response;
         }
     }
     
